f9edba85e9c4340922756a41eef49a8d6cc9a6a6
Anthony <acgardner95@gmail.com>
2022-11-20 21:52:03 -0600

    Add boilerplate scripts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # my-rss-feed
+
+WIP: Pull latest posts from rpilocator, blogs, and other websites
---
+++ scraper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+
+import requests
+
+
+if __name__ == '__main__':
+    print("WIP")
