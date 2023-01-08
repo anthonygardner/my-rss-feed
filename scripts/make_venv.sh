77214d2582d33b2d3d505ed045c2051d595a5e22
Anthony <acgardner95@gmail.com>
2023-01-08 13:28:51 -0800

    Update venv to .venv

--- scripts/make_venv.sh
+++ scripts/make_venv.sh
@@ -3,9 +3,9 @@
 REPO_ROOT="$(pwd)"
 
 echo "Creating a Python virtual environment..."
-python3 -m venv $REPO_ROOT/venv
+python3 -m venv $REPO_ROOT/.venv
 
-source venv/bin/activate
+source .venv/bin/activate
 
 echo "Installing requirements..."
 pip install --upgrade pip
